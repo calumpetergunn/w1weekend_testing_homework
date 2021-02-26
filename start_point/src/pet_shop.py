e15a653aae95c9a27d084ba4ab5a88c2aafaa60a
calumpetergunn <calumgunn@gmail.com>
2021-02-26 17:35:28 +0000

    2 and a half done

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,3 +1,11 @@
 # WRITE YOUR FUNCTIONS HERE
 def get_pet_shop_name(name):
-    
+    return name["name"]
+ 
+def get_total_cash(number):
+    return number["admin"]["total_cash"]
+
+def add_or_remove_cash(number_1, number_2):
+    return number_1["admin"]["total_cash"] + number_2
+
+     
